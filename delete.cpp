d3c74bf4e5dfc7ed9a331deaa50e293cee8a5c2d
yixingup <47037675+yixingup@users.noreply.github.com>
2019-04-25 01:11:20 +0800

    My code is definitely correct!!!

---
+++ delete.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<sstream>
+#include<fstream>
+#include<string>
+#include"Record.h"
+using namespace std;
+void delete(int ID){
+    cin>>ID>>endl;
+    string filename = "records.txt";
+	  ifstream fin( filename.c_str() );
+    if ( fin.fail() ){
+		cout << "Error in file opening!" << endl;
+ 		exit(1);
+ 	  }
+    string line, word,ed;
+    string b[10000];
+    int linecounter=0;
+	  while ( getline(fin, line) ) {
+        b[linecounter]=line;
+        istringstream line_in(line);
+        int id;
+	      line_in >> id;
+        if(id==ID){
+        b[counter]="";
+        }
+       linecounter++;
+   }
+	  fin.close();
+    ofstream fout;
+    fout.open("records.txt");
+    if ( fout.fail() ) {
+      cout << "Error in file opening!"<< endl;
+      exit(1);
+   }
+    for(int i=0;i<linecounter;i++){
+    fout <<b[i]<<endl;
+    }
+    fout.close();
+}
