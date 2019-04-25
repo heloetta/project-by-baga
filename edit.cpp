013f22519a85e91952c194fdcddb91e945e5f2ba
yixingup <47037675+yixingup@users.noreply.github.com>
2019-04-25 23:56:54 +0800

    Update edit.cpp

--- edit.cpp
+++ edit.cpp
@@ -1,62 +1,175 @@
 #include<iostream>
-#include<sstream>
-#include<fstream>
 #include<string>
-#include"Record.h"
-#include"edit.h"
+#include "Record.h"
+#include<fstream>
+#include "edit.h"
+#include "linked_list.h"
+#include<iomanip>
+#include<sstream>
 using namespace std;
-void edit(){
-    int ID;
-    cin>>ID;
-    cout<<endl;
-    string filename = "records.txt";
-	  ifstream fin( filename.c_str() );
-    if ( fin.fail() ){
-		cout << "Error in file opening!" << endl;
- 		exit(1);
- 	  }
-    string line, word,ed;
-    string b[10000];
-    int linecounter=0;
-	  while ( getline(fin, line) ) {
-        b[linecounter]=line;
-        istringstream line_in(line);
-        int id;
-	      line_in >> id;
-        if(id==ID){
-            b[linecounter]="";
-            int counter=0;
-            int choice;
-            cout<<"Edit\n(1.date 2.IO 3.type 4.account 5.amount 6.remark)"<<endl;
-            cin>>choice;
-	    cout<<endl;
-            cout<<"Enter the edited value"<<endl;
-            cin>>ed;
-	    cout<<endl;
-            string a[100];
-            while(line_in>>word){
-            counter++;
-            a[counter]=word;
-            if(counter == choice){
-            a[counter]=ed;
-            }
-            }
-	    counter=0;
-            for(int i=0;i<=6;i++){
-            b[linecounter]=b[linecounter]+a[i]+" ";
-            }
-        }
-       linecounter++;
-   }
-	  fin.close();
-    ofstream fout;
-    fout.open("records.txt");
-    if ( fout.fail() ) {
-      cout << "Error in file opening!"<< endl;
-      exit(1);
-   }
-    for(int i=0;i<linecounter;i++){
-    fout <<b[i]<<endl;
-    }
-    fout.close();
+void edit()
+{
+	int ide;
+	cin>>ide;
+	cout<<endl;
+	ifstream fin;
+	fin.open("records.txt");
+	if(fin.fail())
+	{
+		cout<<"Error in file opening when viewing fin"<<endl;
+		exit(1);
+	}
+	string line;
+	Node * head = NULL,* after_this;
+	//get the sort parameter
+	int sort_id=1;
+	int sort_choice;
+	cin>>sort_choice;
+	while(getline(fin,line))
+	{
+		istringstream iss(line);
+		Record FinRecord;
+		iss>>FinRecord.ID>>FinRecord.date>>FinRecord.IO>>FinRecord.type>>FinRecord.account>>FinRecord.amount>>FinRecord.remark;
+		double sort_pa;
+		switch(sort_choice)
+		{
+		case 1:
+			sort_pa=FinRecord.date;
+			break;
+		case 2:
+			sort_pa=FinRecord.IO;
+			break;
+		case 3:
+			sort_pa=FinRecord.type;
+			break;
+		case 4:
+			sort_pa=FinRecord.account;
+			break;
+		case 5:
+			sort_pa=FinRecord.amount;
+			break;
+		}
+		//build a sorted linked list
+		after_this = find_prev(head,sort_pa);
+		if(after_this == NULL)
+			head_insert(head,FinRecord,sort_pa);
+		else
+			insert(after_this,FinRecord,sort_pa);
+	}
+	fin.close();
+	//end of reading from file
+	//cout and fout (cover the original txt)
+        int choice,ed;
+	string word
+        cout<<"Edit\n(1.date 2.IO 3.type 4.account 5.amount 6.remark)"<<endl;
+        cin>>choice;
+	cout<<endl;
+        cout<<"Enter the edited value"<<endl;
+	if(choice!=6)
+        cin>>ed;
+	else
+	cin>>word;
+	cout<<endl;
+	ofstream fout;
+	fout.open("records.txt");
+	if(fout.fail())
+	{
+		cout<<"Error in file opening when viewing fout"<<endl;
+		exit(1);
+	}
+	cout<<"***************************************************************************"<<endl<<endl;
+	cout<<setw(5)<<"ID"<<setw(9)<<"date"<<" income/expense"<<setw(15)<<"type";
+	cout<<setw(15)<<"account"<<setw(9)<<"amount"<<" remark"<<endl;
+	Node * current = head;
+	while (current != NULL )
+	{
+		Record t = current->info;
+		t.ID=sort_id;
+		cout<<setw(5)<<t.ID<<" "<<setw(8)<<t.date;
+	        if(t.ID==ide){
+		switch(choice){
+		case 1:
+			t.date=ed;
+			break;
+		case 2:
+			t.IO=ed;
+			break;
+		case 3:
+			t.type=ed;
+			break;
+		 case 4:
+			t.account=ed;
+			break;
+		case 5:
+			t.amount=ed;
+			break;
+		case 6:
+			t.remark=word;
+			break;
+			
+			}
+		}
+		switch(t.IO)
+		{
+		case 0:
+			cout<<setw(15)<<"income";
+			break;
+		case 1:
+			cout<<setw(15)<<"expense";
+			break;
+		}
+		switch(t.type)
+		{
+		case 1:
+			cout<<setw(15)<<"food";
+			break;
+		case 2:
+			cout<<setw(15)<<"rent";
+			break;
+		case 3:
+			cout<<setw(15)<<"salary";
+			break;
+		case 4:
+			cout<<setw(15)<<"game";
+			break;
+		case 5:
+			cout<<setw(15)<<"transportation";
+			break;
+		case 6:
+			cout<<setw(15)<<"transfer";
+			break;
+		case 7:
+			cout<<setw(15)<<"other";
+			break;
+		}
+		switch(t.account)
+		{
+		case 1:
+			cout<<setw(15)<<"Alipay";
+			break;
+		case 2:
+			cout<<setw(15)<<"cash";
+			break;
+		case 3:
+			cout<<setw(15)<<"credit card";
+			break;
+		case 4:
+			cout<<setw(15)<<"Octopus";
+			break;
+		case 5:
+			cout<<setw(15)<<"Wechat Pay";
+			break;
+		case 6:
+			cout<<setw(15)<<"other";
+			break;
+		}
+		cout<<setw(9)<<t.amount;
+		cout<<" "<<t.remark<<endl;
+		fout<<t.ID<<" "<<t.date<<" "<<t.IO<<" "<<t.type<<" "<<t.account<<" "<<t.amount<<" "<<t.remark<<endl;
+		current = current->next;
+		sort_id ++;
+	}
+	cout<<endl<<"****************************************************************************"<<endl;
+	fout.close();
+	delete_list(head);
 }
